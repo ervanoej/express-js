ca8519dbd28fd84a63eda87c6ff2b53cc518f77e
ervanoej <41992281+ervanoej@users.noreply.github.com>
2020-06-10 21:12:21 +0700

    Landing page - post route to submit email

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 let landing = require('../controllers/landing')
 /* GET home page. */
-router.get('/', landing.get_landing); 
+router.get('/', landing.get_landing);
+router.post('/', landing.submit_lead); 
 
 module.exports = router;
