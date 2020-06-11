933d17b0b88e1a81f66ef61af43767218a3b74ff
ervanoej <41992281+ervanoej@users.noreply.github.com>
2020-06-11 22:30:41 +0700

    Save email on landing page to database

--- models/leads.js
+++ models/leads.js
@@ -1,9 +1,6 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const Leads = sequelize.define('Leads', {
-    id: DataTypes.INTEGER,
-    createdAt: DataTypes.DATE,
-    updatedAt: DataTypes.DATE,
     email: DataTypes.STRING
   }, {});
   Leads.associate = function(models) {
