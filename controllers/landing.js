dc8e8d0703e8fb2dc24fe6f94cda3884b0369558
ervanoej <41992281+ervanoej@users.noreply.github.com>
2020-06-11 22:53:12 +0700

    - Read and display leads email collection from db

--- controllers/landing.js
+++ controllers/landing.js
@@ -1,13 +1,19 @@
 const models = require('../models');
+
 exports.get_landing = function(req, res, next) {
   res.render('landing', { title: 'Express' });
 };
 
 exports.submit_lead = function(req, res, next) {
-  console.log("lead email: ", req.body.lead_email);
   return models.Leads.create({
     email: req.body.lead_email
   }).then(lead => {
-    res.redirect('/');
+    res.redirect('/leads');
+  })
+};
+
+exports.show_leads = function(req, res, next) {
+  return models.Leads.findAll().then(leads => {
+    res.render('landing', { title: 'Express', leads: leads });
   })
 };
